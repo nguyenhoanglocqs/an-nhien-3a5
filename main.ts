2cb9ec4ac6aace95970d7c02f812c32da3d85152
nguyenhoanglocqs <NGUYENHOANGLOCQS@GMAIL.COM>
2024-11-24 22:56:27 +0700

    Tệp ban đầu của dự án MakeCode

---
+++ main.ts
@@ -0,0 +1,22 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . # . .
+        . # . . .
+        # # # # #
+        . # . . .
+        . . # . .
+        `)
+    basic.pause(500)
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . . # . .
+        . . . # .
+        # # # # #
+        . . . # .
+        . . # . .
+        `)
+    basic.pause(500)
+    basic.clearScreen()
+})
